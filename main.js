67838ae67e286f155e6a0dc5ba6e1a6c7e164993
Phillip Lam <plam89@ucla.edu>
2021-03-13 18:24:07 -0800

    nav bar completed

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-console.log('rail')
+ (function navSlide() {
+   const burger = document.querySelector('.burger')
+   const nav = document.querySelector('.nav-links')
+   const navLinks = document.querySelectorAll('.nav-links li')
+
+
+    // clickin burger icon toggles navbar slide-in on mobile
+   burger.addEventListener('click', () => {
+     nav.classList.toggle('nav-active')
+
+     // using each nav links' index will allow individual links to slide in at different time intervals
+     navLinks.forEach((link, index) => {
+       link.style.animation ? link.style.animation = '' : link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.65}s`
+
+     })
+
+     //burger logo animation
+
+     burger.classList.toggle('toggle')
+   })
+
+
+ })()
+
+
+
+
+
+
+
+
+// const navSlide = () => {
+//   const burger = document.querySelector('.burger')
+//   const nav = document.querySelector('.nav-links')
+
+//   burger.addEventListener('click', () => {
+//     nav.classList.toggle('nav-active')
+//   })
+// }
+
+// navSlide()
