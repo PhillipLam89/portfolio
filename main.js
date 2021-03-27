bb1eed749135bd4db00167512416d5e0dc88c160
Phillip Lam <plam89@ucla.edu>
2021-03-26 21:41:27 -0700

    hamburger toggling completed

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
- (function navSlide() {
-   const burger = document.querySelector('.burger')
-   const nav = document.querySelector('.nav-links')
-   const navLinks = document.querySelectorAll('.nav-links li')
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
 
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
 
-    // clickin burger icon toggles navbar slide-in on mobile
-   burger.addEventListener('click', () => {
-     nav.classList.toggle('nav-active')
-
-     // using each nav links' index will allow individual links to slide in at different time intervals
-     navLinks.forEach((link, index) => {
-       link.style.animation ? link.style.animation = '' : link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.65}s`
-
-     })
-
-     //burger logo animation
-     burger.classList.toggle('toggle')
-   })
- })()
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  })
+})
