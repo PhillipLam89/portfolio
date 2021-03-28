b3b2d5add5d0febb6f4fc8d7f9865cb6c238afef
Phillip Lam <plam89@ucla.edu>
2021-03-27 21:47:31 -0700

    portfolio styles animation and template done

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navItems = document.querySelectorAll('.nav__item')
 const navLinks = document.querySelectorAll('.nav__link')
+const body = document.querySelector('body');
 
 navToggle.addEventListener('click', () => {
+
   document.body.classList.toggle('nav-open');
+  navItems.forEach((link, index) => {
+    link.style.animation ? link.style.animation = '' : link.style.animation = `navLinkFade 1.5s ease forwards ${index + 1 * index / 7 + 1}s`
+  })
+
 });
 
-navLinks.forEach(link => {
+navItems.forEach(link => {
   link.addEventListener('click', () => {
     document.body.classList.remove('nav-open');
+    navItems.forEach(link => {
+      link.style.animation = ''
+    })
   })
+
 })
